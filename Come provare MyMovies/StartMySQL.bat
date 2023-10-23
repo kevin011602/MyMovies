593e7347f2b8f73513f725c653b6e33c03405ab8
kevin011602 <k.paladino@studenti.unisa.it>
2023-10-23 20:16:52 +0200

    Istruzione e file per eseguire e provare MyMovies

---
+++ Come provare MyMovies/StartMySQL.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Avvio del servizio MySQL...
+net start MySQL80
+echo Servizio MySQL avviato con successo!
+exit
---
+++ Come provare MyMovies/StopMySQL.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Spegnimento del servizio MySQL...
+net stop MySQL80
+echo Servizio MySQL spento con successo!
+exit
